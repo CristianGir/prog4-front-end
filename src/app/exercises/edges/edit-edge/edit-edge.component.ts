496472d428584d4a8bfe81544f8bed7621628916
Cristian Giraldo <cgiraldo93759@umanizales.edu.co>
2023-05-16 04:24:48 -0500

    Se activa el método updateEdge y queda funcional.

--- src/app/exercises/edges/edit-edge/edit-edge.component.ts
+++ src/app/exercises/edges/edit-edge/edit-edge.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from "@angular/router";
 import { EdgeService } from "app/shared/services/edge.service";
 import { CityService } from "app/shared/services/city.service";
 import Swal from "sweetalert2/dist/sweetalert2.js";
+import { tap } from "rxjs/operators";
 
 @Component({
   selector: "app-edit-edge",
@@ -11,12 +12,11 @@ import Swal from "sweetalert2/dist/sweetalert2.js";
 })
 export class EditEdgeComponent implements OnInit {
   cities: any = [];
-  edge: any;
+  countOrigin: number;
+  countDestiny: number;
   edgeId: string;
-  originId: string;
   origin: any;
   weight: number;
-  destinyId: string;
   destiny: any;
 
   constructor(
@@ -37,13 +37,21 @@ export class EditEdgeComponent implements OnInit {
     );
   }
   getCity(originId: string, destinyId: string) {
+    let countOrigin = 1;
+    let countDestiny = 1;
     for (let city of this.cities) {
       if (originId === city.cityId) {
         this.origin = city;
+        this.countOrigin = countOrigin;
       }
+      countOrigin++;
+    }
+    for (let city of this.cities) {
       if (destinyId === city.cityId) {
         this.destiny = city;
+        this.countDestiny = countDestiny;
       }
+      countDestiny++;
     }
   }
   ngOnInit() {
@@ -63,41 +71,73 @@ export class EditEdgeComponent implements OnInit {
         });
       } else {
         this.getCity(this.origin, this.destiny);
-        const edge = {
-          edgeId: this.edgeId,
-          origin: this.origin,
-          weight: this.weight,
-          destiny: this.destiny,
-        };
-        this.edgeService.updateEdge(this.edgeId, edge).subscribe(
+        // Primero verifica si ya existe una arista con el mismo origen y destino
+        this.edgeService.getEdges().subscribe(
           (response) => {
             if (response.code === 200) {
-              Swal.fire({
-                title: "Se guardaron los datos",
-                text: "Operación realizada con éxito",
-                icon: "success",
-                confirmButtonText: "Volver",
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  this.router.navigate(["/exercises/edges"]);
-                }
-              });
-            } else {
-              Swal.fire({
-                title: "Error",
-                text: "Un error ha ocurrido",
-                icon: "error",
-                confirmButtonText: "Volver",
-              });
+              const edges = response.data;
+              const existingEdge = edges.find(
+                (edge) =>
+                  (edge.origin.cityId === this.origin.cityId &&
+                    edge.destiny.cityId === this.destiny.cityId) ||
+                  (edge.origin.cityId === this.destiny.cityId &&
+                    edge.destiny.cityId === this.origin.cityId)
+              );
+              if (existingEdge && existingEdge.edgeId !== this.edgeId) {
+                // Ya existe una arista con el mismo origen y destino (excepto la actual)
+                Swal.fire({
+                  title: "Error",
+                  text: "No puede existir más de una arista con la misma ruta",
+                  icon: "error",
+                  confirmButtonText: "Volver",
+                });
+              } else {
+                // No existe una arista con el mismo origen y destino (excepto la actual), se puede actualizar el borde
+                const edge = {
+                  edgeId: this.edgeId,
+                  origin: this.countOrigin,
+                  weight: this.weight,
+                  destiny: this.countDestiny,
+                };
+                this.edgeService
+                  .updateEdge(this.edgeId, edge)
+                  .pipe(
+                    tap(
+                      (response) => {
+                        if (response.code === 200) {
+                          Swal.fire({
+                            title: "Arista actualizada",
+                            text: "Operación realizada con éxito",
+                            icon: "success",
+                            confirmButtonText: "Volver",
+                          }).then((result) => {
+                            if (result.isConfirmed) {
+                              this.router.navigate(["/exercises/edges"]);
+                            }
+                          });
+                        } else {
+                          Swal.fire({
+                            title: "Error",
+                            text: "Un error ha ocurrido",
+                            icon: "error",
+                            confirmButtonText: "Volver",
+                          });
+                        }
+                      },
+                      (error) => {
+                        Swal.fire({
+                          icon: "error",
+                          title: "Error en el servidor",
+                        });
+                      }
+                    )
+                  )
+                  .subscribe();
+              }
             }
           },
           (error) => {
-            Swal.fire({
-              title: "Error en el servidor",
-              text: "Un error ha ocurrido",
-              icon: "error",
-              confirmButtonText: "Volver",
-            });
+            console.log("Error en el servidor.");
           }
         );
       }
@@ -110,6 +150,7 @@ export class EditEdgeComponent implements OnInit {
       });
     }
   }
+
 
   back() {
     this.router.navigate(["/exercises/edges"]);
